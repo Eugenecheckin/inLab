9e3460da8f7fa4eb1d030cf221f7d9ed652afd1f
Evgeniy Shelaykin <Denis>
2022-06-24 18:19:38 +0300

    refactor

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -4,13 +4,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import Auth from './Auth';
 import ChangePass from './ChangePass';
 import Poke from './Poke';
-import SwipeTab from './SwipeTab';
 
 export type RootStackParamList = {
   Auth: undefined;
   Poke: undefined;
   ChangePass: undefined;
-  SwipeTab: undefined;
 };
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -21,7 +19,6 @@ const RootNavigator = () => {
         <Stack.Screen name="Auth" component={Auth} options={{ headerShown: false }} />
         <Stack.Screen name="Poke" component={Poke} options={{ headerShown: false }} />
         <Stack.Screen name="ChangePass" component={ChangePass} options={{ headerShown: false }} />
-        <Stack.Screen name="SwipeTab" component={SwipeTab} />
       </Stack.Navigator>
     </NavigationContainer>
   );
